20184e27b72e16ee5773fb024f93ed4b829b1c14
misyobun <misyobun@booklog.co.jp>
2018-04-03 00:46:51 +0900

    clean up example

--- example.js
+++ example.js
@@ -1,9 +1,3 @@
 const Hitbtc = require("./src/hitbtc.js");
 var hitbtc = new Hitbtc.Hitbtc();
-
-hitbtc.getSymbol("BETETH").then(console.log);
-hitbtc.getOrders("BETETH").then(console.log);
-hitbtc.getTrades("BETETH").then(console.log);
 hitbtc.getCandles("BETETH").then(console.log);
-hitbtc.getTicker("ETHBTC").then(console.log);
-hitbtc.getCurrency("bet").then(console.log);
